0cf55d8ffc9e7073b80adb75b9f0e08daf3a2717
alexggg99 <agashchuk@corp.local>
2018-03-17 17:35:23 +0300

    add account

--- src/main/java/com/onlinebank/ctrl/AccountController.java
+++ src/main/java/com/onlinebank/ctrl/AccountController.java
@@ -11,10 +11,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
@@ -54,6 +58,20 @@ public class AccountController {
         SavingAccount savingAccount = (SavingAccount) accountService.getSavingAccount(id, principal.getName());
         complementResponce(model, savingAccount, filter, principal, page);
         return "account";
+    }
+
+    @GetMapping("/add")
+    public String addAccount(Principal principal) {
+        List<Currency> currHoldByUser = accountService.getAllAccounts(principal.getName()).stream()
+                .filter(elem -> elem instanceof PrimaryAccount)
+                .map(elem -> ((PrimaryAccount) elem).getCurrency()).collect(Collectors.toList());
+        List<Currency> filtered = Arrays.asList(Currency.values()).stream()
+                .filter(elem ->  !currHoldByUser.contains(elem))
+                .collect(Collectors.toList());
+        if (filtered.size() > 0) {
+            accountService.createPrimaryAccount(filtered.get(0), principal.getName());
+        }
+        return "redirect:/primaryAccounts";
     }
 
     private void complementResponce(Model model, Account account, String filter, Principal principal, int page){
